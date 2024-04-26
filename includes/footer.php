154dcebee22cf746bb543d6be48152fcafeb7faf
Michael Erameh <eramehmichael2000@gmail.com>
2024-04-26 12:16:10 +0100

    change

--- includes/footer.php
+++ includes/footer.php
@@ -87,6 +87,7 @@
 
 <!-- Template Main JS File -->
 <script src="assets/js/main.js"></script>
+<?php include 'livechat.com';?>
 </body>
 
 </html>
---
+++ includes/livechat.php
@@ -0,0 +1,18 @@
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '6f77801ef8c4c31ddd4869e6c967ff1754f90e5b';
+window.smartsupp || (function(d) {
+    var s, c, o = smartsupp = function() {
+        o._.push(arguments)
+    };
+    o._ = [];
+    s = d.getElementsByTagName('script')[0];
+    c = d.createElement('script');
+    c.type = 'text/javascript';
+    c.charset = 'utf-8';
+    c.async = true;
+    c.src = 'https://www.smartsuppchat.com/loader.js?';
+    s.parentNode.insertBefore(c, s);
+})(document);
+</script>
